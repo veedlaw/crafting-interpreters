5e1d1965ea17686b6bb174ff270b8d25d886b8c9
Walter Herold Veedla <walterheroldv@gmail.com>
2023-11-18 16:17:37 +0100

    Add Scanner.java. Implement single character token parsing

--- jlox/src/Scanner.java
+++ jlox/src/Scanner.java
@@ -1,2 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+    private boolean isAtEnd = false;
+
+    public Scanner(String source) {
+        this.source = source;
+    }
+
+    public List<Token> scanTokens() {
+        // Keep scanning tokens until we have consumed all characters
+        // from the source string
+        while (!isAtEnd()) {
+            // We are at the beginning of the next lexeme
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            // Single character tokens
+            case '(': addToken(TokenType.LEFT_PAREN); break;
+            case ')': addToken(TokenType.RIGHT_PAREN); break;
+            case '{': addToken(TokenType.LEFT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            case '.': addToken(TokenType.DOT); break;
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case ';': addToken(TokenType.SEMICOLON); break;
+            case '/': addToken(TokenType.SLASH); break;
+            case '*': addToken(TokenType.STAR); break;
+
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
+        }
+    }
+
+    /*
+    Consume a character from the source and return it.
+    Advances the current source character index as a side effect.
+     */
+    private char advance() {
+        char next_char = source.charAt(current);
+        current++;
+        return next_char;
+    }
+
+    /*
+
+     */
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
 }
