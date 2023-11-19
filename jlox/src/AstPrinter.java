02f328475940658842cfe5f8e713cf0d2811fa71
Walter Herold Veedla <walterheroldv@gmail.com>
2023-11-19 20:15:33 +0100

    Complete implementation of AstPrinter.java

--- jlox/src/AstPrinter.java
+++ jlox/src/AstPrinter.java
@@ -1,22 +1,52 @@
 public class AstPrinter implements Expr.Visitor<String> {
 
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
     @Override
     public String visitBinaryExpr(Expr.Binary expr) {
-        return null;
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
     }
 
     @Override
     public String visitGroupingExpr(Expr.Grouping expr) {
-        return null;
+        return parenthesize("group", expr.expression);
     }
 
     @Override
     public String visitLiteralExpr(Expr.Literal expr) {
-        return null;
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
     }
 
     @Override
     public String visitUnaryExpr(Expr.Unary expr) {
-        return null;
+        return parenthesize(expr.operator.lexeme, expr.right);
     }
+
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("(").append(name);
+        for (Expr expr : exprs) {
+            stringBuilder.append(" ");
+            stringBuilder.append(expr.accept(this));
+        }
+        stringBuilder.append(")");
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Literal(45.67)));
+
+        System.out.println(new AstPrinter().print(expression));
+    }
+
 }
