2471cdffba6648933bd51e51f2e8f94162575a0e
Walter Herold Veedla <walterheroldv@gmail.com>
2023-12-17 16:49:35 +0100

    Add literal value interpretation

--- jlox/src/Interpreter.java
+++ jlox/src/Interpreter.java
@@ -1,2 +1,7 @@
-public class Interpreter {
+public class Interpreter implements Expr.Visitor<Object> {
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
 }
